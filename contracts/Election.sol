f9c8958b41e86efa4929c3417e44e2a4c5b015e7
David Yzaguirre <dvdyzag@gmail.com>
2018-04-17 22:30:46 -0600

    Added contract and migration

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,1 +1,11 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.2;
+
+contract Election {
+  // Store candidate
+  // Read candidate
+  string public candidate;
+  // Constructor
+  function Election() public {
+    candidate = "Candidate 1";
+  }
+}
